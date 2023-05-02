7ab8059a2c42172ca2f1da8eacdc0576ccae1ac3
podhmo <ababjam61+github@gmail.com>
2023-05-02 22:39:26 +0900

    Builder prototype (#1)
    * feat: simple 2 fields
    
    * feat: two types fields
    
    * feat: string field impl, integer field impl
    
    * feat: adding String type and Integer type
    
    * feat: field's method also returns specialized field
    
    * test: add tests
    
    * feat: introduce UntypedField
    
    * refactor: rename structs
    
    * feat: array type
    
    * -
    
    * feat: remove untyped field
    
    * refactor: use embedded
    
    * feat: ObjectType
    
    * refactor: use embedded generics type_[R].ret
    
    * refactor: move interface definition to top
    
    * feat: write type

---
+++ builder/builder.go
@@ -0,0 +1,329 @@
+package builder
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func New() *Builder {
+	return &Builder{}
+}
+
+type TypeBuilder interface {
+	typevalue() *Type
+	WriteType(io.Writer) error
+}
+type FieldBuilder interface {
+	fieldvalue() *Field
+}
+
+type Builder struct {
+	// TODO: storing types
+}
+
+func (b *Builder) Object(fields ...FieldBuilder) *ObjectType {
+	t := &ObjectType{
+		ObjectBuilder: &ObjectBuilder[*ObjectType]{
+			type_: &type_[*ObjectType]{value: &Type{Name: "object"}},
+			value: &Object{Fields: fields},
+		},
+	}
+	t.ObjectBuilder.ret = t
+	return t
+}
+
+type ObjectType struct {
+	*ObjectBuilder[*ObjectType]
+}
+
+func (b *Builder) Field(name string, typ TypeBuilder) *TypedField {
+	f := &TypedField{
+		field: &field[*TypedField]{
+			value: &Field{Name: name, Required: true},
+		},
+		typ: typ,
+	}
+	f.field.ret = f
+	return f
+}
+
+func (b *Builder) Array(typ TypeBuilder) *ArrayType[TypeBuilder] { // TODO: specialized
+	t := &ArrayType[TypeBuilder]{ArrayBuilder: &ArrayBuilder[TypeBuilder, *ArrayType[TypeBuilder]]{
+		type_: &type_[*ArrayType[TypeBuilder]]{value: &Type{Name: "array"}},
+		items: typ,
+		value: &Array{},
+	}}
+	t.ArrayBuilder.ret = t
+	return t
+}
+
+type ArrayType[T TypeBuilder] struct {
+	*ArrayBuilder[T, *ArrayType[T]]
+}
+
+func (b *Builder) Map(valtyp TypeBuilder) *MapType[TypeBuilder] { // TODO: specialized
+	t := &MapType[TypeBuilder]{MapBuilder: &MapBuilder[TypeBuilder, *MapType[TypeBuilder]]{
+		type_: &type_[*MapType[TypeBuilder]]{value: &Type{Name: "map[string]"}},
+		items: valtyp,
+		value: &Map{},
+	}}
+	t.MapBuilder.ret = t
+	return t
+}
+
+type MapType[T TypeBuilder] struct {
+	*MapBuilder[T, *MapType[T]]
+}
+
+func (b *Builder) String() *StringType {
+	t := &StringType{StringBuilder: &StringBuilder[*StringType]{
+		type_: &type_[*StringType]{value: &Type{Name: "string"}},
+		value: &String{},
+	}}
+	t.StringBuilder.ret = t
+	return t
+}
+
+type StringType struct {
+	*StringBuilder[*StringType]
+}
+
+func (b *Builder) Integer() *IntegerType {
+	t := &IntegerType{IntegerBuilder: &IntegerBuilder[*IntegerType]{
+		type_: &type_[*IntegerType]{value: &Type{Name: "integer"}},
+		value: &Integer{},
+	}}
+	t.IntegerBuilder.ret = t
+	return t
+}
+
+type IntegerType struct {
+	*IntegerBuilder[*IntegerType]
+}
+
+type type_[R any] struct {
+	value *Type
+	ret   R
+}
+
+func (t *type_[R]) typevalue() *Type {
+	return t.value
+}
+func (t *type_[R]) WriteType(w io.Writer) error {
+	if _, err := io.WriteString(w, t.value.Name); err != nil {
+		return err
+	}
+	return nil
+}
+func (t *type_[R]) Doc(stmts ...string) R {
+	t.value.Description = strings.Join(stmts, "\n")
+	return t.ret
+}
+func (t *type_[R]) Format(v string) R {
+	t.value.Format = v
+	return t.ret
+}
+func (t *type_[R]) As(name string) R {
+	t.value.Name = name
+	t.value.IsNewType = true
+	return t.ret
+}
+
+type Type struct {
+	Name        string
+	Description string
+	Format      string
+
+	IsNewType bool
+}
+
+type Field struct {
+	Name        string
+	Description string
+	Required    bool
+}
+
+type field[R any] struct {
+	value *Field
+	ret   R
+}
+
+func (f *field[R]) fieldvalue() *Field {
+	return f.value
+}
+func (t *field[R]) Doc(stmts ...string) R {
+	t.value.Description = strings.Join(stmts, "\n")
+	return t.ret
+}
+
+func (t *field[R]) Required(v bool) R {
+	t.value.Required = v
+	return t.ret
+}
+
+var _ FieldBuilder = (*field[any])(nil)
+
+type TypedField struct {
+	*field[*TypedField]
+	typ TypeBuilder
+}
+
+// https://swagger.io/docs/specification/data-models/data-types/
+
+type StringBuilder[R any] struct {
+	*type_[R]
+	value *String
+}
+
+var _ TypeBuilder = (*StringBuilder[any])(nil)
+
+func (t *StringBuilder[R]) MinLength(n int64) R {
+	t.value.MinLength = n
+	return t.ret
+}
+func (t *StringBuilder[R]) MaxLength(n int64) R {
+	t.value.MaxLength = n
+	return t.ret
+}
+func (t *StringBuilder[R]) Pattern(s string) R {
+	t.value.Pattern = s
+	return t.ret
+}
+
+type String struct {
+	MinLength int64
+	MaxLength int64
+	Pattern   string
+}
+
+type IntegerBuilder[R any] struct {
+	*type_[R]
+	value *Integer
+}
+
+var _ TypeBuilder = (*IntegerBuilder[any])(nil)
+
+func (t *IntegerBuilder[R]) Minimum(n int64) R {
+	t.value.Minimum = n
+	return t.ret
+}
+func (t *IntegerBuilder[R]) Maximum(n int64) R {
+	t.value.Maximum = n
+	return t.ret
+}
+
+type Integer struct {
+	// minimum ≤ value ≤ maximum
+	Maximum int64
+	Minimum int64
+}
+
+// composite type
+type ObjectBuilder[R any] struct {
+	*type_[R]
+	value *Object
+}
+
+func (b ObjectBuilder[R]) WriteType(w io.Writer) error {
+	if err := b.type_.WriteType(w); err != nil {
+		return err
+	}
+	if b.type_.value.IsNewType {
+		return nil
+	}
+
+	io.WriteString(w, "{") // nolint
+	n := len(b.value.Fields) - 1
+	for i, f := range b.value.Fields {
+		v := f.fieldvalue()
+		io.WriteString(w, v.Name) // nolint
+		if !v.Required {
+			io.WriteString(w, "?") // nolint
+		}
+		if i < n {
+			io.WriteString(w, ", ") // nolint
+		}
+	}
+	io.WriteString(w, "}") // nolint
+	return nil
+}
+
+func (b *ObjectBuilder[R]) String(v bool) R {
+	b.value.Strict = v
+	return b.ret
+}
+
+type Object struct {
+	Strict bool
+
+	Fields []FieldBuilder
+}
+
+type ArrayBuilder[T TypeBuilder, R any] struct {
+	*type_[R]
+	items T
+	value *Array
+}
+
+func (t *ArrayBuilder[T, R]) WriteType(w io.Writer) error {
+	if err := t.type_.WriteType(w); err != nil {
+		return err
+	}
+	io.WriteString(w, "[") // nolint
+	if err := t.items.WriteType(w); err != nil {
+		return err
+	}
+	io.WriteString(w, "]") // noint
+	return nil
+}
+
+func (t *ArrayBuilder[T, R]) MinItems(n int64) R {
+	t.value.MinItems = n
+	return t.ret
+}
+func (t *ArrayBuilder[T, R]) MaxItems(n int64) R {
+	t.value.MaxItems = n
+	return t.ret
+}
+
+type Array struct {
+	MaxItems int64
+	MinItems int64
+}
+
+// string only map
+type MapBuilder[V TypeBuilder, R any] struct {
+	*type_[R]
+	items V
+	value *Map
+}
+
+func (t *MapBuilder[V, R]) WriteType(w io.Writer) error {
+	if err := t.type_.WriteType(w); err != nil {
+		return err
+	}
+	io.WriteString(w, "[") // nolint
+	if err := t.items.WriteType(w); err != nil {
+		return err
+	}
+	io.WriteString(w, "]") // noint
+	return nil
+}
+
+func (t *MapBuilder[T, R]) PatternProperties(s string) R {
+	t.value.PatternProperties = s
+	return t.ret
+}
+
+type Map struct {
+	PatternProperties string
+}
+
+func ToString(typ TypeBuilder) string {
+	b := new(strings.Builder)
+	if err := typ.WriteType(b); err != nil {
+		return fmt.Sprintf("invalid type: %T", typ)
+	}
+	return b.String()
+}
---
+++ builder/builder_test.go
@@ -0,0 +1,55 @@
+package builder_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/podhmo/gos/builder"
+)
+
+func TestIt(*testing.T) {
+	b := builder.New()
+
+	Person := b.Object(
+		b.Field("name", b.String().MinLength(1).MaxLength(255)),
+		b.Field("age", b.Integer().Minimum(0).Doc("hoho")).Required(true).Doc("haha"),
+		b.Field("skills", b.Array(b.String().Doc("yaya").MinLength(1)).MinItems(1)).Required(false),
+	).As("Person").Doc(
+		"this is summary",
+		"",
+		"this is long description\nhehehhe",
+	)
+	fmt.Println(Person)
+}
+
+func TestToString(t *testing.T) {
+	b := builder.New()
+
+	tests := []struct {
+		name string
+		typ  builder.TypeBuilder
+		want string
+	}{
+		{"primitive-0", b.String(), "string"},
+		{"primitive-1", b.Integer(), "integer"},
+		{"new-type-primitive", b.String().As("Name"), "Name"},
+		{"array-string", b.Array(b.String()), "array[string]"},
+		{"array-array-string", b.Array(b.Array(b.String())), "array[array[string]]"},
+		{"object", b.Object(
+			b.Field("name", b.String()).Required(true),
+			b.Field("age", b.String()).Required(false),
+		), "object{name, age?}"},
+		{"new-type-object", b.Object(
+			b.Field("name", b.String()).Required(true),
+			b.Field("age", b.String()).Required(false),
+		).As("Person"), "Person"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := builder.ToString(tt.typ)
+			if got != tt.want {
+				t.Errorf("ToString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
