e1e93077ce450ea6fd845f1f34e408d937d20ca4
podhmo <ababjam61+github@gmail.com>
2023-05-04 19:38:35 +0900

    fix: integration

--- builder/_examples/main.go
+++ builder/_examples/main.go
@@ -10,22 +10,22 @@ import (
 func main() {
 	b := builder.New()
 
-	Name := b.String().MinLength(1).As("Name")
+	Name := builder.Define("Name", b.String().MinLength(1))
 
-	b.Object(
+	builder.Define("Person", b.Object(
 		b.Field("name", b.String()).Doc("name of person"),
 		b.Field("age", b.Integer().Format("int32")),
 		b.Field("nickname", b.Reference(Name)).Required(false),
 		b.Field("father", b.ReferenceByName("Person")).Required(false),
 		b.Field("friends", b.Array(b.ReferenceByName("Person"))).Required(false),
-	).As("Person").Doc("person object")
+	)).Doc("person object")
 
-	b.Object(
+	builder.Define("TestScore", b.Object(
 		b.Field("title", b.String()),
 		b.Field("tests", b.Map(b.Integer()).
 			PatternProperties(`\-score$`, b.Integer().Doc("score (0~100)")).
 			PatternProperties(`\-grade$`, b.String().Doc("grade (A,B,C,D,E,F)"))),
-	).As("TestScore")
+	))
 
 	doc, err := builder.ToSchema(b)
 	if err != nil {
--- builder/builder_test.go
+++ builder/builder_test.go
@@ -11,12 +11,12 @@ func TestIt(*testing.T) {
 	b := builder.New()
 
 	typeName := "Person"
-	b.Object(
+	builder.Define(typeName, b.Object(
 		b.Field("name", b.String().MinLength(1).MaxLength(255)),
 		b.Field("age", b.Integer().Minimum(0).Doc("hoho")).Required(true).Doc("haha"),
 		b.Field("skills", b.Array(b.String().Doc("yaya").MinLength(1)).MinItems(1)).Required(false), // composite
 		b.Field("father", b.ReferenceByName(typeName)),                                              // recursive
-	).As(typeName).Doc(
+	)).Doc(
 		"this is summary",
 		"",
 		"this is long description\nhehehhe",
@@ -38,17 +38,17 @@ func TestToString(t *testing.T) {
 	}{
 		{"primitive-0", b.String(), "string"},
 		{"primitive-1", b.Integer(), "integer"},
-		{"new-type-primitive", b.String().As("Name"), "Name"},
+		{"new-type-primitive", builder.Define("Name", b.String()), "Name"},
 		{"array-string", b.Array(b.String()), "array[string]"},
 		{"array-array-string", b.Array(b.Array(b.String())), "array[array[string]]"},
 		{"object", b.Object(
 			b.Field("name", b.String()),
 			b.Field("age", b.String()).Required(false),
 		), "object{name, age?}"},
-		{"new-type-object", b.Object(
+		{"new-type-object", builder.Define("Person", b.Object(
 			b.Field("name", b.String()),
 			b.Field("age", b.String()).Required(false),
-		).As("Person"), "Person{name, age?}"},
+		)), "Person{name, age?}"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
