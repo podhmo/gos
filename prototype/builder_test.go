6a304a627bdf5780fb6f692c86228246ff441664
podhmo <ababjam61+github@gmail.com>
2023-05-04 20:50:46 +0900

    refactor: rename package /builder -> /prototype

--- prototype/builder_test.go
+++ prototype/builder_test.go
@@ -1,16 +1,16 @@
-package builder_test
+package prototype_test
 
 import (
 	"testing"
 
-	"github.com/podhmo/gos/builder"
+	"github.com/podhmo/gos/prototype"
 )
 
 func TestIt(t *testing.T) {
-	b := builder.New()
+	b := prototype.NewBuilder()
 
 	typeName := "Person"
-	builder.Define(typeName, b.Object(
+	prototype.Define(typeName, b.Object(
 		b.Field("name", b.String().MinLength(1).MaxLength(255)),
 		b.Field("age", b.Integer().Minimum(0).Doc("hoho")).Required(true).Doc("haha"),
 		b.Field("skills", b.Array(b.String().Doc("yaya").MinLength(1)).MinItems(1)).Required(false), // composite
@@ -21,8 +21,8 @@ func TestIt(t *testing.T) {
 		"this is long description\nhehehhe",
 	)
 
-	b.EachTypes(func(typ builder.TypeBuilder) error {
-		got := builder.ToString(typ)
+	b.EachTypes(func(typ prototype.TypeBuilder) error {
+		got := prototype.ToString(typ)
 		want := `Person{name, age, skills?, father?}`
 		if want != got {
 			t.Errorf("want %q, but got %q", want, got)
@@ -32,30 +32,30 @@ func TestIt(t *testing.T) {
 }
 
 func TestToString(t *testing.T) {
-	b := builder.New()
+	b := prototype.NewBuilder()
 
 	tests := []struct {
 		name string
-		typ  builder.TypeBuilder
+		typ  prototype.TypeBuilder
 		want string
 	}{
 		{"primitive-0", b.String(), "string"},
 		{"primitive-1", b.Integer(), "integer"},
-		{"new-type-primitive", builder.Define("Name", b.String()), "Name"},
+		{"new-type-primitive", prototype.Define("Name", b.String()), "Name"},
 		{"array-string", b.Array(b.String()), "array[string]"},
 		{"array-array-string", b.Array(b.Array(b.String())), "array[array[string]]"},
 		{"object", b.Object(
 			b.Field("name", b.String()),
 			b.Field("age", b.String()).Required(false),
 		), "object{name, age?}"},
-		{"new-type-object", builder.Define("Person", b.Object(
+		{"new-type-object", prototype.Define("Person", b.Object(
 			b.Field("name", b.String()),
 			b.Field("age", b.String()).Required(false),
 		)), "Person{name, age?}"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := builder.ToString(tt.typ)
+			got := prototype.ToString(tt.typ)
 			if got != tt.want {
 				t.Errorf("ToString() = %v, want %v", got, tt.want)
 			}
