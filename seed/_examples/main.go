709e2f5f31906d2e0084bdf0cdb0b8f05c225226
podhmo <ababjam61+github@gmail.com>
2023-05-14 02:19:40 +0900

    chore: more metadata in main

--- seed/_examples/main.go
+++ seed/_examples/main.go
@@ -18,29 +18,46 @@ func run() error {
 	cmd := seed.NewCommand(os.Args[1:])
 	options := cmd.Config
 
-	goStringType := seed.Symbol("string") // unexported string is go-primitive
-
 	// define
 	b := seed.NewBuilder(options.PkgName)
 	b.NeedReference()
 
 	Type := b.BuildTarget("Type")
-	b.TargetField("Format", goStringType, `json:"format"`)
+	b.TargetField("Format", seed.Symbol("string"), `json:"format"`)
 
+	Bool := b.Type("Bool").
+		NeedBuilder().Underlying("boolean")
 	Int := b.Type("Int").
+		Field("Maximum", seed.Symbol("int64"), `json:"maximum,omitempty"`).
+		Field("Minimum", seed.Symbol("int64"), `json:"minimum,omitempty"`).
 		NeedBuilder().Underlying("intger")
 	String := b.Type("String").
+		Field("Pattern", seed.Symbol("string"), `json:"pattern,omitempty"`).
+		Field("MaxLength", seed.Symbol("int64"), `json:"maxlength,omitempty"`).
+		Field("MinLength", seed.Symbol("int64"), `json:"minlength,omitempty"`).
 		NeedBuilder().Underlying("string")
 
 	Array := b.Type("Array", seed.TypeVar{Name: "Items", Type: seed.Symbol("TypeBuilder")}).
+		Field("MaxItems", seed.Symbol("int64"), `json:"maxitems,omitempty"`).
+		Field("MinItems", seed.Symbol("int64"), `json:"minitems,omitempty"`).
 		NeedBuilder().Underlying("array")
 	Map := b.Type("Map", seed.TypeVar{Name: "Items", Type: seed.Symbol("TypeBuilder")}).
+		Field("Pattern", seed.Symbol("string"), `json:"pattern,omitempty"`).
 		NeedBuilder().Underlying("map")
 
 	Field := b.Type("Field").
+		Field("Name", seed.Symbol("string"), `json:"-"`).
+		Field("Typ", seed.Symbol("TypeBuilder"), `json:"-"`).
+		Field("Description", seed.Symbol("string"), `json:"description,omitempty"`).
+		Field("Required", seed.Symbol("bool"), `json:"-"`).
+		Constructor(
+			seed.Arg{Name: "Name", Type: seed.Symbol("string")},
+			seed.Arg{Name: "Typ", Type: seed.Symbol("TypeBuilder")},
+		).
 		NeedBuilder().Underlying("field") //?
 	Object := b.Type("Object").
 		Field("Fields", seed.Symbol("[]*FieldType"), `json:"-"`).
+		Field("Strict", seed.Symbol("bool"), `json:"-"`).
 		Constructor(seed.Arg{Name: "Fields", Type: seed.Symbol("*FieldType"), Variadic: true}).
 		NeedBuilder().Underlying("object")
 
@@ -56,7 +73,7 @@ func run() error {
 	// 	Field("Output", ActionOutput.Metadata.Name, "").
 	// 	NeedBuilder()
 
-	fmt.Fprintln(os.Stderr, Type, Int, String, Array, Map, Field, Object)
+	fmt.Fprintln(os.Stderr, Type, Bool, Int, String, Array, Map, Field, Object)
 	// fmt.Fprintln(os.Stderr, Param, ActionInput, ActionOutput, Action)
 	// fmt.Fprintln(os.Stderr, b.Metadata.Types)
 
