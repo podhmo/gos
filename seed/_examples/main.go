044f3d5b3017781dbd1d8790d794a87cacc0aa6e
podhmo <ababjam61+github@gmail.com>
2023-05-13 17:03:52 +0900

    feat: use flags in main.go

--- seed/_examples/main.go
+++ seed/_examples/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"text/template"
 
 	"github.com/podhmo/gos/seed"
 )
 
+var options struct {
+	Builder  bool
+	Metadata bool
+
+	All bool
+}
+
 func main() {
+	flag.BoolVar(&options.Builder, "builder", false, "emit builder.go")
+	flag.BoolVar(&options.Metadata, "metadata", false, "emit builder.go")
+	flag.BoolVar(&options.All, "all", false, "emit all")
+	flag.Parse()
+
+	if options.All {
+		options.Builder = true
+		options.Metadata = true
+	}
+
+	if err := run(); err != nil {
+		log.Fatalf("!! %+v", err)
+	}
+}
+
+func run() error {
 	b := seed.NewBuilder()
 
 	Type := b.BuildTarget("Type")
@@ -38,13 +63,19 @@ func main() {
 		fmt.Fprintln(os.Stderr, b.Metadata.Types)
 		t := template.Must(template.New("").Parse(tmpl))
 
-		if err := t.ExecuteTemplate(os.Stdout, "Builder", b.Metadata); err != nil {
-			panic(err)
+		if options.Builder {
+			fmt.Fprintln(os.Stderr, "--builder.go----------------------------------------")
+			if err := t.ExecuteTemplate(os.Stdout, "Builder", b.Metadata); err != nil {
+				panic(err)
+			}
 		}
 
-		fmt.Println("----------------------------------------")
-		if err := t.ExecuteTemplate(os.Stdout, "Metadata", b.Metadata); err != nil {
-			panic(err)
+		if options.Metadata {
+			fmt.Fprintln(os.Stderr, "--metadata.go----------------------------------------")
+			if err := t.ExecuteTemplate(os.Stdout, "Metadata", b.Metadata); err != nil {
+				panic(err)
+			}
 		}
 	}
+	return nil
 }
