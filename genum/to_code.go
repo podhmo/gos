ef5b343210a8a54a1c1008c893b9affbff0909be
podhmo <ababjam61+github@gmail.com>
2023-05-13 20:26:41 +0900

    fix: fix go code

--- genum/to_code.go
+++ genum/to_code.go
@@ -3,17 +3,17 @@ package genum
 import (
 	"fmt"
 	"io"
-	"reflect"
+	"strings"
 )
 
 type writeCoder interface {
 	writeCode(io.Writer) error
 }
 
-func WriteCode[T any](w io.Writer, b *Builder[T]) error {
-	return b.EachTypes(func(typ TypeBuilder[T]) error {
+func WriteCode(w io.Writer, b *Builder) error {
+	return b.EachEnums(func(typ EnumBuilder) error {
 		if err := typ.writeCode(w); err != nil {
-			return fmt.Errorf("%s: %w", typ.GetTypeMetadata().Name, err)
+			return fmt.Errorf("%s: %w", typ.GetEnumMetadata().Name, err)
 		}
 		fmt.Fprintln(w, "")
 		return nil
@@ -21,13 +21,14 @@ func WriteCode[T any](w io.Writer, b *Builder[T]) error {
 }
 
 // customization
+func (t *StringEnum) writeCode(w io.Writer) error {
+	// padding := t.rootbuilder.Config.Padding
+	// comment := t.rootbuilder.Config.Comment
+	padding := "\t"
+	comment := "//"
 
-func (t *EnumType[T]) writeCode(w io.Writer) error {
-	padding := t.rootbuilder.Config.Padding
-	comment := t.rootbuilder.Config.Comment
-
-	typename := t.type_.metadata.Name
-	underlying := t.type_.metadata.underlying
+	typename := t._Enum.metadata.Name
+	underlying := t._Enum.metadata.underlying
 
 	fmt.Fprintf(w, "%s %s", comment, typename) // nolint
 	// TODO: description
@@ -37,16 +38,17 @@ func (t *EnumType[T]) writeCode(w io.Writer) error {
 	fmt.Fprintln(w, "")
 	fmt.Fprintln(w, "const (")
 
-	isString := reflect.TypeOf(t.Values[0].metadata.Value).Kind() == reflect.String
-
-	for _, v := range t.Values {
-		if isString {
-			fmt.Fprintf(w, "%s%s%s %s = \"%v\"", padding, typename, v.metadata.Name, typename, v.metadata.Value)
-		} else {
-			fmt.Fprintf(w, "%s%s%s %s = %v", padding, typename, v.metadata.Name, typename, v.metadata.Value)
+	for _, v := range t.metadata.Members {
+		name := v.Name
+		if name == "" {
+			name = toTitle(v.Value)
 		}
 
-		if v.metadata.Default {
+		if v.Doc != "" {
+			fmt.Fprintf(w, "%s%s %q\n", padding, comment, v.Doc)
+		}
+		fmt.Fprintf(w, "%s%s%s %s = %q", padding, typename, name, typename, v.Value)
+		if t.metadata.Default == v.Value {
 			fmt.Fprintf(w, "  %s default", comment)
 		}
 		fmt.Fprintln(w, "")
@@ -55,6 +57,42 @@ func (t *EnumType[T]) writeCode(w io.Writer) error {
 	return nil
 }
 
-func (t *type_[T, R]) writeCode(w io.Writer) error {
-	panic("never")
+func (t *IntEnum) writeCode(w io.Writer) error {
+	// padding := t.rootbuilder.Config.Padding
+	// comment := t.rootbuilder.Config.Comment
+	padding := "\t"
+	comment := "//"
+
+	typename := t._Enum.metadata.Name
+	underlying := t._Enum.metadata.underlying
+
+	fmt.Fprintf(w, "%s %s", comment, typename) // nolint
+	// TODO: description
+	fmt.Fprintln(w, "")
+	fmt.Fprintf(w, "type %s %s\n", typename, underlying) // nolint
+
+	fmt.Fprintln(w, "")
+	fmt.Fprintln(w, "const (")
+
+	for _, v := range t.metadata.Members {
+		name := v.Name
+
+		if v.Doc != "" {
+			fmt.Fprintf(w, "%s%s %q\n", padding, comment, v.Doc)
+		}
+		fmt.Fprintf(w, "%s%s%s %s = %v", padding, typename, name, typename, v.Value)
+		if t.metadata.Default == v.Value {
+			fmt.Fprintf(w, "  %s default", comment)
+		}
+		fmt.Fprintln(w, "")
+	}
+	fmt.Fprintln(w, ")")
+	return nil
+}
+
+func toTitle(s string) string {
+	if s == "" {
+		return s
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
 }
