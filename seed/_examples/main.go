5a0de2d9f9b7b91f5b8a82409275a09cdf063f05
podhmo <ababjam61+github@gmail.com>
2023-05-14 01:50:13 +0900

    chore: update main.go

--- seed/_examples/main.go
+++ seed/_examples/main.go
@@ -20,28 +20,39 @@ func run() error {
 
 	// define
 	b := seed.NewBuilder(options.PkgName)
+	b.NeedReference()
 
 	Type := b.BuildTarget("Type")
-	goStringType := seed.Symbol("string") // unexported string is go-primitive
+	// goStringType := seed.Symbol("string") // unexported string is go-primitive
 
 	Int := b.Type("Int").NeedBuilder()
 	String := b.Type("String").NeedBuilder()
 
-	Array := b.Type("Array").Var("typ", Type).NeedBuilder()
-	Map := b.Type("Map").Var("valtype", Type).NeedBuilder()
+	Array := b.Type("Array", seed.TypeVar{Name: "Items", Type: seed.Symbol("TypeBuilder")}).NeedBuilder()
+	Map := b.Type("Map", seed.TypeVar{Name: "Items", Type: seed.Symbol("TypeBuilder")}).NeedBuilder()
 
-	Field := b.Type("Field")
-	FieldType := Field.Metadata.Name
-	Object := b.Type("Object").Field("Fields", FieldType).NeedBuilder()
+	Field := b.Type("Field").
+		NeedBuilder()
+	Object := b.Type("Object").
+		Field("Fields", seed.Symbol("[]*FieldType"), `json:"-"`).
+		Constructor(seed.Arg{Name: "Fields", Type: seed.Symbol("*FieldType"), Variadic: true}).
+		NeedBuilder()
 
-	Param := b.Type("Param")
-	ParamType := Param.Metadata.Name
-	ActionInput := b.Type("ActionInput").Field("Params", ParamType)
-	ActionOutput := b.Type("ActionOutput").Field("Return", Type)
-	Action := b.Type("Action").Field("Name", goStringType).Field("Input", ActionInput.Metadata.Name).Field("Output", ActionOutput.Metadata.Name).NeedBuilder()
+	// Param := b.Type("Param")
+	// ParamType := Param.Metadata.Name
+	// ActionInput := b.Type("ActionInput").
+	// 	Field("Params", ParamType, "")
+	// ActionOutput := b.Type("ActionOutput").
+	// 	Field("Return", Type, "")
+	// Action := b.Type("Action").
+	// 	Field("Name", goStringType, "").
+	// 	Field("Input", ActionInput.Metadata.Name, "").
+	// 	Field("Output", ActionOutput.Metadata.Name, "").
+	// 	NeedBuilder()
 
-	fmt.Fprintln(os.Stderr, Type, Int, String, Array, Map, Field, Object, Param, ActionInput, ActionOutput, Action)
-	fmt.Fprintln(os.Stderr, b.Metadata.Types)
+	fmt.Fprintln(os.Stderr, Type, Int, String, Array, Map, Field, Object)
+	// fmt.Fprintln(os.Stderr, Param, ActionInput, ActionOutput, Action)
+	// fmt.Fprintln(os.Stderr, b.Metadata.Types)
 
 	// emit
 	return cmd.Do(b)
