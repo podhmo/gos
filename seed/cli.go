8da8bc5b2666d3bb77ea87e9434b77dc4e6ec2d8
podhmo <ababjam61+github@gmail.com>
2023-05-20 00:52:54 +0900

    feat: add to_string parts in seed

--- seed/cli.go
+++ seed/cli.go
@@ -26,6 +26,7 @@ type Config struct {
 
 	Builder  bool
 	Metadata bool
+	ToString bool
 
 	All   bool
 	Write bool
@@ -38,6 +39,7 @@ func NewCommand(args []string) *Command {
 	fs.StringVar(&config.PkgName, "pkgname", "M", "package {{.PkgName}}")
 	fs.BoolVar(&config.Builder, "builder", false, "emit builder.go")
 	fs.BoolVar(&config.Metadata, "metadata", false, "emit metadata.go")
+	fs.BoolVar(&config.ToString, "to-string", false, "emit to_string.go")
 	fs.BoolVar(&config.All, "all", false, "emit all")
 	fs.BoolVar(&config.Write, "write", false, "write file")
 
@@ -45,6 +47,7 @@ func NewCommand(args []string) *Command {
 	if config.All {
 		config.Builder = true
 		config.Metadata = true
+		config.ToString = true
 	}
 
 	funcMap := template.FuncMap{
@@ -88,6 +91,22 @@ func (c *Command) Do(b *Builder) error {
 		}
 		if err := t.ExecuteTemplate(w, "Metadata", b.metadata); err != nil {
 			return fmt.Errorf("write metadata.go: %w", err)
+		}
+	}
+
+	if options.ToString {
+		fmt.Fprintln(os.Stderr, "--to_string.go----------------------------------------")
+		var w io.Writer = os.Stdout
+		if options.Write {
+			f, err := os.Create("to_string.go")
+			if err != nil {
+				return fmt.Errorf("create to_string.go: %w", err)
+			}
+			defer f.Close()
+			w = f
+		}
+		if err := t.ExecuteTemplate(w, "ToString", b.metadata); err != nil {
+			return fmt.Errorf("write to_string.go: %w", err)
 		}
 	}
 	return nil
