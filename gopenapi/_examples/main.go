09a2359d4aa21a5ba9f50f82998f75a581de876c
podhmo <ababjam61+github@gmail.com>
2023-05-20 00:07:59 +0900

    chore: update example

--- gopenapi/_examples/main.go
+++ gopenapi/_examples/main.go
@@ -11,48 +11,6 @@ import (
 )
 
 func main() {
-	b := gopenapi.NewTypeBuilder()
-
-	Name := gopenapi.DefineType("Name", b.String().MinLength(1))
-
-	Person := gopenapi.DefineType("Person", b.Object(
-		b.Field("name", b.String()).Doc("name of person"),
-		b.Field("age", b.Int().Format("int32")),
-		b.Field("nickname", b.Reference(Name)).Required(false),
-		b.Field("father", b.ReferenceByName("Person")).Required(false),
-		b.Field("friends", b.Array(b.ReferenceByName("Person"))).Required(false),
-	)).Doc("person object")
-
-	PersonSummary := gopenapi.DefineType("PersonSummary", b.Object(
-		Person.OnlyFields("name", "nickname")...,
-	)).Doc("person objec summary")
-
-	TestScore := gopenapi.DefineType("TestScore", b.Object(
-		b.Field("title", b.String()),
-		b.Field("tests", b.Map(b.Int()).Pattern(`\-score$`).Doc("score (0~100)")),
-	))
-
-	// enum, in production, import from other package
-	var orderingEnum *genum.String
-	{
-		b := genum.NewEnumBuilder(genum.DefaultConfig())
-		orderingEnum = b.String(
-			b.StringValue("desc").Doc("降順"),
-			b.StringValue("asc").Doc("昇順"),
-		).Default("desc").Doc("順序")
-	}
-	Ordering := gopenapi.DefineType("Ordering", b.StringFromEnum(orderingEnum))
-
-	// TODO:
-	// Hello :: func(name string) string
-	Hello := b.Action("hello",
-		b.Input(
-			b.Param("name", b.String(), "path"),
-		).Doc("input"),
-		b.Output(
-			b.String(),
-		),
-	).Doc("greeting hello")
 
 	doc, err := gopenapi.ToSchema(b)
 	if err != nil {
@@ -77,25 +35,81 @@ func main() {
 	// routing
 	{
 		r := gopenapi.NewRouter()
-		{
-			r := r.Tagged("greeting")
-			r.Post("/hello", Hello)
-		}
-		{
-			r := r.Tagged("people")
-			r.Get("/people", b.Action("ListPerson",
-				b.Input(
-					b.Query("sort", b.String().Enum([]string{"name", "-name", "age", "-age"})),
-				),
-				b.Output(b.Array(PersonSummary)),
-			).Doc("list person"))
-		}
-
+		mount(r)
 		doc := orderedmap.New()
 		r.ToSchemaWith(b, doc)
-
 		if err := enc.Encode(doc); err != nil {
 			panic(err)
 		}
 	}
 }
+
+func mount(r *gopenapi.Router) {
+	{
+		r := r.Tagged("greeting")
+		r.Post("/hello", Hello)
+	}
+	{
+		r := r.Tagged("people")
+		r.Get("/people", ListPerson)
+	}
+}
+
+var b = gopenapi.NewTypeBuilder()
+
+var (
+	Name = gopenapi.DefineType("Name", b.String().MinLength(1))
+
+	Person = gopenapi.DefineType("Person", b.Object(
+		b.Field("name", b.String()).Doc("name of person"),
+		b.Field("age", b.Int().Format("int32")),
+		b.Field("nickname", b.Reference(Name)).Required(false),
+		b.Field("father", b.ReferenceByName("Person")).Required(false),
+		b.Field("friends", b.Array(b.ReferenceByName("Person"))).Required(false),
+	)).Doc("person object")
+
+	PersonSummary = gopenapi.DefineType("PersonSummary", b.Object(
+		Person.OnlyFields("name", "nickname")...,
+	)).Doc("person objec summary")
+
+	TestScore = gopenapi.DefineType("TestScore", b.Object(
+		b.Field("title", b.String()),
+		b.Field("tests", b.Map(b.Int()).Pattern(`\-score$`).Doc("score (0~100)")),
+	))
+
+	Ordering *gopenapi.String
+)
+
+func init() {
+	// enum, in production, import from other package
+	var orderingEnum *genum.String
+	{
+		b := genum.NewEnumBuilder(genum.DefaultConfig())
+		orderingEnum = b.String(
+			b.StringValue("desc").Doc("降順"),
+			b.StringValue("asc").Doc("昇順"),
+		).Default("desc").Doc("順序")
+	}
+
+	Ordering = gopenapi.DefineType("Ordering", b.StringFromEnum(orderingEnum))
+}
+
+// actions
+var (
+	// Hello :: func(name string) string
+	Hello = b.Action("hello",
+		b.Input(
+			b.Param("name", b.String(), "path"),
+		).Doc("input"),
+		b.Output(
+			b.String(),
+		),
+	).Doc("greeting hello")
+
+	ListPerson = b.Action("ListPerson",
+		b.Input(
+			b.Query("sort", b.String().Enum([]string{"name", "-name", "age", "-age"})),
+		),
+		b.Output(b.Array(PersonSummary)),
+	).Doc("list person")
+)
