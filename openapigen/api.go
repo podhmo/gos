c98f7f41b90385b38f560145c250804c2fce375f
podhmo <ababjam61+github@gmail.com>
2023-05-26 14:09:20 +0900

    feat: Config field

--- openapigen/api.go
+++ openapigen/api.go
@@ -3,8 +3,15 @@
 package openapigen
 
 type Config struct {
+	DisableRefLinks bool // if true, does not use $ref links
+
+	defs []toSchemer
+	seen map[toSchemer]bool // TODO: name conflict?
 }
 
 func DefaultConfig() *Config {
-	return &Config{}
+	return &Config{
+		DisableRefLinks: false,
+		seen:            map[toSchemer]bool{},
+	}
 }
