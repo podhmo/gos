356f6341833b9a1d9e4f7be13e43651f6a0725eb
podhmo <ababjam61+github@gmail.com>
2023-06-04 23:52:44 +0900

    chore: update examples

--- openapigen/_examples/02separated/main.go
+++ openapigen/_examples/02separated/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/podhmo/gos/openapigen"
 	design "github.com/podhmo/gos/openapigen/_examples/02separated/design"
 	action "github.com/podhmo/gos/openapigen/_examples/02separated/design/action"
+	"github.com/podhmo/gos/pkg/maplib"
 )
 
 func main() {
@@ -29,7 +30,20 @@ func main() {
 	}
 
 	// emit
-	doc := orderedmap.New()
+	doc, _ := maplib.Merge(orderedmap.New(), &openapigen.OpenAPI{
+		OpenAPI: "3.0.3",
+		Info: openapigen.Info{
+			Title:   "task API",
+			Version: "0.0.0",
+			Doc:     "simple list tasks API",
+		},
+		Servers: []openapigen.Server{
+			{
+				URL: "http://localhost:8080",
+				Doc: "local development",
+			},
+		},
+	})
 	r.ToSchemaWith(b, doc)
 
 	enc := json.NewEncoder(os.Stdout)
