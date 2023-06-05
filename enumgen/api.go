d902e6579286a2ad8ec132304eccdda4a1e09396
podhmo <ababjam61+github@gmail.com>
2023-06-06 00:14:05 +0900

    refactor: ToGoCode() as as Config method

--- enumgen/api.go
+++ enumgen/api.go
@@ -2,9 +2,70 @@
 //go:generate go fmt .
 package enumgen
 
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"strings"
+	"text/template"
+)
+
 type Config struct {
+	FuncMap  template.FuncMap
+	Template *template.Template
 }
 
 func DefaultConfig() *Config {
-	return &Config{}
+	c := &Config{
+		FuncMap: template.FuncMap{
+			"splitLines": func(s string) []string {
+				return strings.Split(s, "\n")
+			},
+			"toType": func(t EnumBuilder) string {
+				return t.GetEnumMetadata().underlying
+			},
+			"toTitle": toTitle,
+		},
+	}
+	c.Template = template.Must(template.New("").Funcs(c.FuncMap).Parse(tmpl))
+	return c
+}
+
+//go:embed gocode.tmpl
+var tmpl string
+
+func (c *Config) ToGoCode(w io.Writer, tb EnumBuilder) error {
+	switch tb := tb.(type) {
+	case *String:
+		for _, v := range tb.metadata.Members {
+			if v.metadata.Name == "" {
+				v.metadata.Name = toTitle(v.metadata.Value)
+			}
+		}
+		if err := c.Template.ExecuteTemplate(w, "String", tb); err != nil {
+			return fmt.Errorf("execute template: %w", err)
+		}
+	case *Int:
+		if err := c.Template.ExecuteTemplate(w, "Int", tb); err != nil {
+			return fmt.Errorf("execute template: %w", err)
+		}
+	default:
+		panic(fmt.Sprintf("unexpected type: %T", tb))
+	}
+	return nil
+}
+
+func ToGocode(w io.Writer, b *Builder) error {
+	return b.EachEnums(func(tb EnumBuilder) error {
+		return b.Config.ToGoCode(w, tb)
+	})
+}
+
+// helpers
+
+func toTitle(s string) string {
+	if s == "" {
+		return s
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
 }
