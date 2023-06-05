17125d78dfebec0ca1f4b7f7865cc5143bd0f91d
podhmo <ababjam61+github@gmail.com>
2023-06-05 23:11:54 +0900

    chore: update example (enumgen)

---
+++ enumgen/_examples/testdata/generated.go
@@ -0,0 +1,21 @@
+
+// Ordering : 順序
+type Ordering string
+
+const (
+	// "降順"
+	OrderingDesc Ordering = "desc"  // default
+	// "昇順"
+	OrderingAsc Ordering = "asc"
+)
+
+
+type Season int
+
+const (
+	SeasonSpring Season = 0  // default
+	SeasonSummer Season = 1
+	SeasonAutumn Season = 2
+	SeasonWinter Season = 3
+)
+
