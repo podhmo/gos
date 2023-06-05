4c9e6e56a518990e88cbb79b2549a9198c518a83
podhmo <ababjam61+github@gmail.com>
2023-06-05 23:10:33 +0900

    chore: update example

--- openapigen/_examples/03tocode/testdata/generated.go
+++ openapigen/_examples/03tocode/testdata/generated.go
@@ -13,38 +13,26 @@ type Friends []Person
 
 
 type Tag struct {
-
-    
     // name of tag
     Name Name `json:"name"`
 
-    
     Doc string `json:"doc"`
-
 }
 
 
 
 // person object
 type Person struct {
-
-    
     // name of person
     Name Name `json:"name"`
 
-    
     Age int64 `json:"age"`
 
-    
     Father *Person `json:"father"`
 
-    
     Children []Person `json:"children"`
 
-    
     Friends Friends `json:"friends"`
 
-    
     Tags map[string]Tag `json:"tags"`
-
 }
