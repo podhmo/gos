c4b2bd07641cfe88e3d68ced74e2a703235799b0
podhmo <ababjam61+github@gmail.com>
2023-06-05 21:27:40 +0900

    feat: object

---
+++ openapigen/to_code.go
@@ -0,0 +1,43 @@
+package openapigen
+
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"strings"
+	"text/template"
+)
+
+//go:embed to_code.tmpl
+var tmpl string
+
+func ToGocode(w io.Writer, b *Builder) error {
+	funcMap := template.FuncMap{
+		"toTitle": func(s string) string { // TODO: goify
+			if s == "" {
+				return ""
+			}
+			return strings.ToUpper(s[:1]) + s[1:]
+		},
+		"toType": func(t Type) string {
+			return t.GetTypeMetadata().underlying
+		},
+		"splitLines": func(s string) []string {
+			return strings.Split(s, "\n")
+		},
+	}
+	t, err := template.New("").Funcs(funcMap).Parse(tmpl)
+	if err != nil {
+		return fmt.Errorf("load template: %w", err)
+	}
+
+	return b.EachTypes(func(tb TypeBuilder) error {
+		switch tb := tb.(type) {
+		case *Object:
+			if err := t.ExecuteTemplate(w, "Object", tb); err != nil {
+				return fmt.Errorf("execute template: %w", err)
+			}
+		}
+		return nil
+	})
+}
