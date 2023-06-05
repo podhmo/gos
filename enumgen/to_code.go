d3b00346bfca9f6efd1eb5fd48ee7706f7a19c7b
podhmo <ababjam61+github@gmail.com>
2023-06-05 23:51:05 +0900

    refactor: use text/template

--- enumgen/to_code.go
+++ enumgen/to_code.go
@@ -1,110 +1,51 @@
 package enumgen
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"strings"
+	"text/template"
 )
 
-type writeCoder interface {
-	writeCode(io.Writer) error
-}
+//go:embed to_code.tmpl
+var tmpl string
 
-func WriteCode(w io.Writer, b *Builder) error {
-	return b.EachEnums(func(typ EnumBuilder) error {
-		t, ok := typ.(writeCoder)
-		if !ok {
-			return nil
-		}
+func ToGocode(w io.Writer, b *Builder) error {
+	funcMap := template.FuncMap{
+		"splitLines": func(s string) []string {
+			return strings.Split(s, "\n")
+		},
+		"toType": func(t EnumBuilder) string {
+			return t.GetEnumMetadata().underlying
+		},
+		"toTitle": toTitle,
+	}
+	t, err := template.New("").Funcs(funcMap).Parse(tmpl)
+	if err != nil {
+		return fmt.Errorf("load template: %w", err)
+	}
 
-		if err := t.writeCode(w); err != nil {
-			return fmt.Errorf("%s: %w", typ.GetEnumMetadata().Name, err)
+	return b.EachEnums(func(tb EnumBuilder) error {
+		switch tb := tb.(type) {
+		case *String:
+			for _, v := range tb.metadata.Members {
+				if v.Name == "" {
+					v.Name = toTitle(v.Value)
+				}
+			}
+			if err := t.ExecuteTemplate(w, "String", tb); err != nil {
+				return fmt.Errorf("execute template: %w", err)
+			}
+		case *Int:
+			if err := t.ExecuteTemplate(w, "Int", tb); err != nil {
+				return fmt.Errorf("execute template: %w", err)
+			}
+		default:
+			panic(fmt.Sprintf("unexpected type: %T", tb))
 		}
-		fmt.Fprintln(w, "")
 		return nil
 	})
-}
-
-// customization
-func (t *String) writeCode(w io.Writer) error {
-	padding := t.rootbuilder.Config.Padding // "\t"
-	comment := t.rootbuilder.Config.Comment // //
-
-	typename := t._Enum.metadata.Name
-	underlying := t._Enum.metadata.underlying
-
-	// TODO: description
-	fmt.Fprintln(w, "")
-	if doc := t._Enum.metadata.Doc; doc != "" {
-		for i, line := range strings.Split(doc, "\n") {
-			if i == 0 {
-				fmt.Fprintln(w, comment, typename, ":", line)
-			} else {
-				fmt.Fprintln(w, comment, line)
-			}
-		}
-	}
-	fmt.Fprintf(w, "type %s %s\n", typename, underlying) // nolint
-
-	fmt.Fprintln(w, "")
-	fmt.Fprintln(w, "const (")
-
-	for _, v := range t.metadata.Members {
-		name := v.Name
-		if name == "" {
-			name = toTitle(v.Value)
-		}
-
-		if v.Doc != "" {
-			fmt.Fprintf(w, "%s%s %q\n", padding, comment, v.Doc)
-		}
-		fmt.Fprintf(w, "%s%s%s %s = %q", padding, typename, name, typename, v.Value)
-		if t.metadata.Default == v.Value {
-			fmt.Fprintf(w, "  %s default", comment)
-		}
-		fmt.Fprintln(w, "")
-	}
-	fmt.Fprintln(w, ")")
-	return nil
-}
-
-func (t *Int) writeCode(w io.Writer) error {
-	padding := t.rootbuilder.Config.Padding // "\t"
-	comment := t.rootbuilder.Config.Comment // //
-
-	typename := t._Enum.metadata.Name
-	underlying := t._Enum.metadata.underlying
-
-	// TODO: description
-	fmt.Fprintln(w, "")
-	if doc := t._Enum.metadata.Doc; doc != "" {
-		for i, line := range strings.Split(doc, "\n") {
-			if i == 0 {
-				fmt.Fprintln(w, comment, typename, ":", line)
-			} else {
-				fmt.Fprintln(w, comment, line)
-			}
-		}
-	}
-	fmt.Fprintf(w, "type %s %s\n", typename, underlying) // nolint
-
-	fmt.Fprintln(w, "")
-	fmt.Fprintln(w, "const (")
-
-	for _, v := range t.metadata.Members {
-		name := v.Name
-
-		if v.Doc != "" {
-			fmt.Fprintf(w, "%s%s %q\n", padding, comment, v.Doc)
-		}
-		fmt.Fprintf(w, "%s%s%s %s = %v", padding, typename, name, typename, v.Value)
-		if t.metadata.Default == v.Value {
-			fmt.Fprintf(w, "  %s default", comment)
-		}
-		fmt.Fprintln(w, "")
-	}
-	fmt.Fprintln(w, ")")
-	return nil
 }
 
 func toTitle(s string) string {
