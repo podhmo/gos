6e388d03145081d87ae91e577cf3c81707954970
podhmo <ababjam61+github@gmail.com>
2023-06-06 16:42:31 +0900

    chore: use go's doc string

--- openapigen/_examples/02separated/design/actions.go
+++ openapigen/_examples/02separated/design/actions.go
@@ -1,6 +1,9 @@
 package design
 
 import (
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"runtime"
 	"strings"
 
@@ -15,38 +18,34 @@ type Handler struct {
 	b *openapigen.Builder
 }
 
-func callerName() string {
-	pc, _, _, _ := runtime.Caller(1)
-	rfunc := runtime.FuncForPC(pc)
-	parts := strings.Split(rfunc.Name(), ".")
-	return parts[len(parts)-1]
-}
-
-// Hello :: func(name string) string
+// greeting hello
 func (h *Handler) Hello() *openapigen.Action {
-	return b.Action(callerName(),
+	info := callerInfo()
+	return b.Action(info.Name,
 		b.Input(
 			b.Param("name", b.String()).AsPath(),
 		).Doc("input"),
 		b.Output(
 			b.String(),
 		),
-	).Doc("greeting hello")
+	).Doc(info.Doc)
 }
 
-// ListPerson :: func(...) []PersonSummary
+// list person
 func (h *Handler) ListPerson() *openapigen.Action {
-	return b.Action(callerName(),
+	info := callerInfo()
+	return b.Action(info.Name,
 		b.Input(
 			b.Param("sort", b.String().Enum([]string{"name", "-name", "age", "-age"})).AsQuery(),
 		),
 		b.Output(b.Array(PersonSummary)).Doc("list of person summary"),
-	).Doc("list person")
+	).Doc(info.Doc)
 }
 
-// CreatePerson :: func(...) ()
+// create person
 func (h *Handler) CreatePerson() *openapigen.Action {
-	return b.Action(callerName(),
+	info := callerInfo()
+	return b.Action(info.Name,
 		b.Input(
 			b.Param("verbose", b.Bool()).AsQuery(),
 			b.Body(b.Object(
@@ -56,5 +55,48 @@ func (h *Handler) CreatePerson() *openapigen.Action {
 			)).Doc("person but father and friends are id"),
 		),
 		b.Output(nil).Status(204),
-	).Doc("create person")
+	).Doc(info.Doc)
+}
+
+var (
+	astCache = map[string]*ast.File{}
+	fset     = token.NewFileSet()
+)
+
+func callerInfo() (info struct {
+	Name   string
+	File   string
+	Lineno int
+	Doc    string
+}) {
+	pc, _, _, _ := runtime.Caller(1)
+	rfunc := runtime.FuncForPC(pc)
+	file, lineno := rfunc.FileLine(pc)
+	f, ok := astCache[file]
+	if !ok {
+		tree, err := parser.ParseFile(fset, file, nil, parser.ParseComments|parser.SkipObjectResolution)
+		if err != nil {
+			panic(err)
+		}
+		astCache[file] = tree
+		f = tree
+	}
+
+	pos := fset.File(f.Pos()).LineStart(lineno)
+	for _, decl := range f.Decls {
+		decl, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+		if decl.Pos() <= pos && pos <= decl.End() {
+			info.Doc = strings.TrimSpace(decl.Doc.Text())
+			break
+		}
+	}
+	parts := strings.Split(rfunc.Name(), ".")
+	info.Name = parts[len(parts)-1]
+
+	info.File = file
+	info.Lineno = lineno
+	return
 }
