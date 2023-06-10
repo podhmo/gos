d1c9bb1b0c5ca79b8e17ca08b075eed751399165
podhmo <ababjam61+github@gmail.com>
2023-06-11 03:48:39 +0900

    chore: add example

---
+++ openapigen/_examples/05oneof/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/iancoleman/orderedmap"
+	"github.com/podhmo/gos/openapigen"
+)
+
+func main() {
+	b := openapigen.NewBuilder(openapigen.DefaultConfig())
+
+	// https://swagger.io/docs/specification/data-models/oneof-anyof-allof-not/#oneof
+
+	Dog := openapigen.Define("Dog", b.Object(
+		b.Field("bark", b.Bool()),
+		b.Field("breed", b.String().Enum([]string{"Dingo", "Husky", "Retriever", "Sheperd"})),
+	))
+
+	Cat := openapigen.Define("Cat", b.Object(
+		b.Field("hunts", b.Bool()),
+		b.Field("age", b.Int()),
+	))
+
+	UpdatePet := b.Action("UpdatePet",
+		b.Input(b.Body(b.OneOf(Cat, Dog))),
+		b.Output(nil).Doc("Updated"),
+	)
+
+	r := openapigen.NewRouter(b.Object())
+	r.Patch("/pets", UpdatePet)
+
+	doc := orderedmap.New()
+	if err := r.ToSchemaWith(b, doc); err != nil {
+		panic(err)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(doc); err != nil {
+		panic(err)
+	}
+}
