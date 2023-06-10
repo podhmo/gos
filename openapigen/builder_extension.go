0349745294d8b69bacc728ec78d6db88235b4ccd
podhmo <ababjam61+github@gmail.com>
2023-06-11 03:46:18 +0900

    feat: OneOf() returns Type instead of TypeBuilder

--- openapigen/builder_extension.go
+++ openapigen/builder_extension.go
@@ -61,13 +61,13 @@ func (b *Builder) IntFromEnum(enum *enumgen.Int) *Int {
 	return typ
 }
 
-func (b *Builder) OneOf(types []Type) TypeBuilder {
+func (b *Builder) OneOf(types ...Type) Type {
 	return b._Container().Op("oneOf").Types(types)
 }
-func (b *Builder) AllOf(types []Type) TypeBuilder {
+func (b *Builder) AllOf(types ...Type) Type {
 	return b._Container().Op("allOf").Types(types)
 }
-func (b *Builder) AnyOf(types []Type) TypeBuilder {
+func (b *Builder) AnyOf(types ...Type) Type {
 	return b._Container().Op("anyOf").Types(types)
 }
 
