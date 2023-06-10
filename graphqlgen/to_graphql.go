cc64b87ed04e5217d30abdbffcde7b9a2cb1beb7
podhmo <ababjam61+github@gmail.com>
2023-06-10 23:26:29 +0900

    wip

--- graphqlgen/to_graphql.go
+++ graphqlgen/to_graphql.go
@@ -1,7 +1,50 @@
 package graphqlgen
 
-import "io"
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/podhmo/gos/pkg/namelib"
+)
+
+//go:embed graphql.tmpl
+var tmpl string
+
+func graphqlType(t Type) string {
+	return t.GetTypeMetadata().underlying
+}
 
 func ToGraphql(w io.Writer, b *Builder) error {
-	return nil
+	funcMap := template.FuncMap{
+		"toTitle": namelib.ToTitle,
+		"toGraphqlType": func(t Type) string {
+			return graphqlType(t)
+		},
+		"splitLines": func(s string) []string {
+			return strings.Split(s, "\n")
+		},
+	}
+	t, err := template.New("").Funcs(funcMap).Parse(tmpl)
+	if err != nil {
+		return err
+	}
+
+	w = os.Stderr
+	return b.EachTypes(func(tb TypeBuilder) error {
+		switch tb := tb.(type) {
+		case *Object:
+			if err := t.ExecuteTemplate(w, "Object", tb); err != nil {
+				return fmt.Errorf("execute template: %w", err)
+			}
+		default:
+			if err := t.ExecuteTemplate(w, "Type", tb); err != nil {
+				return fmt.Errorf("execute template: %w", err)
+			}
+		}
+		return nil
+	})
 }
