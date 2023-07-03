d4a1f2f2fa3dac8d31fdc4f9886aac1995485228
podhmo <ababjam61+github@gmail.com>
2023-07-03 16:01:09 +0900

    chore: update example

--- confgen/_examples/00tutorial/main.go
+++ confgen/_examples/00tutorial/main.go
@@ -14,16 +14,20 @@ func main() {
 
 	Product := confgen.Define("Product", b.Object(
 		b.Field("productId", b.Int()).Doc(`The unique identifier for a product`),
-	))
+		b.Field("productName", b.String()).Doc(`Name of the product`),
+		b.Field("price", b.Float()).Doc(`The price of the product`, "(TODO: exclsiveMinimum: 0)"),
+		b.Field("tags", b.Array(b.String()).MinItems(1).UniqueItems(true)).Required(false).Doc(`tags for the product`),
+	)).Doc(`A product from Acme's catalog`)
 
 	doc, err := confgen.ToJSONSchema(b, Product)
 	if err != nil {
 		panic(err)
 	}
+	doc.Set("$id", "https://example.com/product.schema.json")
 
 	w := os.Stdout
 	enc := json.NewEncoder(w)
-	enc.SetIndent("", "  ")
+	enc.SetIndent("", "    ")
 	if err := enc.Encode(doc); err != nil {
 		panic(err)
 	}
