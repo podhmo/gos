e990d7c72397da7701e808d3d966f7a57252f6c6
podhmo <ababjam61+github@gmail.com>
2023-07-11 16:28:53 +0900

    feat: togocode

--- confgen/api.go
+++ confgen/api.go
@@ -3,10 +3,20 @@
 package confgen
 
 import (
+	"fmt"
+	"io"
+	"strings"
+	"text/template"
+
+	_ "embed"
+
 	"github.com/iancoleman/orderedmap"
+	"github.com/podhmo/gos/pkg/namelib"
 )
 
 type Config struct {
+	FuncMap  template.FuncMap
+	Template *template.Template
 
 	// for to schema
 	defs []TypeBuilder
@@ -16,7 +26,20 @@ type Config struct {
 func DefaultConfig() *Config {
 	c := &Config{
 		seen: map[int]*TypeRef{},
+		FuncMap: template.FuncMap{
+			"toTitle": namelib.ToTitle,
+			"toType": func(t Type) string {
+				return typeString(t, false)
+			},
+			"toTypeInternal": func(t Type) string {
+				return typeString(t, true)
+			},
+			"splitLines": func(s string) []string {
+				return strings.Split(s, "\n")
+			},
+		},
 	}
+	c.Template = template.Must(template.New("").Funcs(c.FuncMap).Parse(tmpl))
 	return c
 }
 
@@ -25,4 +48,47 @@ func ToJSONSchema(b *Builder, typ Type) (*orderedmap.OrderedMap, error) {
 	doc.Set("$schema", "http://json-schema.org/draft-07/schema#")
 	useRef := false
 	return ToSchemaWith(doc, b, typ, useRef)
+}
+
+//go:embed gocode.tmpl
+var tmpl string
+
+func typeString(t Type, internal bool) string {
+	metadata := t.GetTypeMetadata()
+	if !internal {
+		if named := metadata.id > 0; named {
+			return metadata.Name
+		}
+	}
+
+	switch impl := t.(type) {
+	case *Array[Type]:
+		return fmt.Sprintf("[]%s", typeString(impl.items, false))
+	case *Map[Type]:
+		return fmt.Sprintf("map[string]%s", typeString(impl.items, false))
+	default:
+		return metadata.goType
+	}
+}
+
+func (c *Config) ToGoCode(w io.Writer, tb TypeBuilder) error {
+	t := c.Template
+	switch tb := tb.(type) {
+	case *Object:
+		if err := t.ExecuteTemplate(w, "Object", tb); err != nil {
+			return fmt.Errorf("execute template: %w", err)
+		}
+	default:
+		if err := t.ExecuteTemplate(w, "Type", tb); err != nil {
+			return fmt.Errorf("execute template: %w", err)
+		}
+	}
+	return nil
+}
+
+func ToGoCode(w io.Writer, b *Builder) error {
+	c := b.Config
+	return b.EachTypes(func(tb TypeBuilder) error {
+		return c.ToGoCode(w, tb)
+	})
 }
